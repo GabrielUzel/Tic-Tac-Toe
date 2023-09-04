a3f5c48e610f83f08de858dd937391ebc1af8d9f
GabrielUzel <gabriel.uzel1@gmail.com>
2023-09-04 13:01:12 -0300

    Moving the game functions to the prototype

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,61 @@
 const startButton = document.querySelector("#start-game-button");
 const gameMap = document.querySelector("#game-map");
+const gameButtons = document.querySelectorAll(".game-button"); // Each area of the game has a button, defined here:
 
-function factoryGame() {
-    return {
-        turn : 1,
-        // Each area of the game has a button, defined here:
-        gameButtons: document.querySelectorAll(".game-button"),
-        // Game buttons => 0 = not selected; 1 = X; 2 = O
-        gameButtonsState: [0, 0, 0, 0, 0, 0, 0, 0, 0], // Array to verify the winner
-
-        startGame() {
-            this.turn = 1 // 1 = X; 2 = O
-
-            gameMap.addEventListener("click", element => {
-                this.clickButton(element.target);
-            })
-        },
-
-        clickButton(button) {
-            if(button.id === "game-button-0") this.selectButton(this.turn, 0);
-            if(button.id === "game-button-1") this.selectButton(this.turn, 1);
-            if(button.id === "game-button-2") this.selectButton(this.turn, 2);
-            if(button.id === "game-button-3") this.selectButton(this.turn, 3);
-            if(button.id === "game-button-4") this.selectButton(this.turn, 4);
-            if(button.id === "game-button-5") this.selectButton(this.turn, 5);
-            if(button.id === "game-button-6") this.selectButton(this.turn, 6);
-            if(button.id === "game-button-7") this.selectButton(this.turn, 7);
-            if(button.id === "game-button-8") this.selectButton(this.turn, 8);
-        },
-
-        selectButton(turn, buttonNumber) { 
-            if(turn === 1) {
-                this.gameButtons[buttonNumber].innerText = "X"
-                this.turn = 2;
-            } else {
-                this.gameButtons[buttonNumber].innerText = "O"
-                this.turn = 1;
-            }
-        },
-
-        checkWin() {
-            return 0;
-        },
-
-        reloadGame() {
-            for(button of this.gameButtons) {
-                button.innerText = "";
-            }
-        }
-    };
+function Game() {
+    this.turn = 1;
+    this.gameButtonsState = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // Array to verify the winner 
+    // Game buttons => 0 = not selected; 1 = X; 2 = O
 }
 
-const game = factoryGame();
+Game.prototype.startGame = () => {
+    turn = 1; // 1 = X; 2 = O
+
+    for(button of gameButtons) {
+        button.setAttribute("onclick", "game.clickButton(this)");
+    }
+
+
+};
+
+Game.prototype.clickButton = (button) => {
+    if(button.id === "game-button-0") game.selectButton(this.turn, 0);
+    if(button.id === "game-button-1") game.selectButton(this.turn, 1);
+    if(button.id === "game-button-2") game.selectButton(this.turn, 2);
+    if(button.id === "game-button-3") game.selectButton(this.turn, 3);
+    if(button.id === "game-button-4") game.selectButton(this.turn, 4);
+    if(button.id === "game-button-5") game.selectButton(this.turn, 5);
+    if(button.id === "game-button-6") game.selectButton(this.turn, 6);
+    if(button.id === "game-button-7") game.selectButton(this.turn, 7);
+    if(button.id === "game-button-8") game.selectButton(this.turn, 8);
+};
+
+Game.prototype.selectButton = (turn, buttonNumber) => {
+    if(turn === 1) {
+        gameButtons[buttonNumber].innerText = "X"
+        game.gameButtonsState[buttonNumber] = 1;
+        gameButtons[buttonNumber].removeAttribute("onclick");
+        this.turn = 2;
+    } else {
+        gameButtons[buttonNumber].innerText = "O";
+        game.gameButtonsState[buttonNumber] = 2;
+        gameButtons[buttonNumber].removeAttribute("onclick");
+        this.turn = 1;
+    }
+};
+
+Game.prototype.checkWin = () => {
+    return 0;
+};
+
+Game.prototype.reloadGame = () => {
+    for(button of this.gameButtons) {
+        button.innerText = "";
+        gameButtonsState = gameButtonsState.map(() => 0);
+    }
+};
+
+const game = new Game();
 startButton.addEventListener("click", e => {
     e.preventDefault();
     game.startGame();
